9d3b82ccddd09da4fa8f56b77b54312dd84a7967
LuckyRathod <luckyrathod46@gmail.com>
2023-09-24 13:05:30 +0530

    Final Changes DL-TF-DVC

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ DVC project for DL usecase using tensorflow
 
 ## Project structure -
 
-<img src="docs/images/DVC-CNN-pipeline@2x (1).png" alt="workflow" width="70%">
+<img src="https://github.com/c17hawke/DVC-CNN-TF-pipeline-demo/blob/main/docs/images/DVC-CNN-pipeline@2x%20(1).png?raw=true" alt="workflow" width="70%">
 
 
 ## STAGE 1: Steps
--- setup.py
+++ setup.py
@@ -4,21 +4,26 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "AIOPS-DL-Pipeline-CNN-TF"
+REPO_NAME = "AIOPS-DL-PIPELINE-CNN-TF"
 AUTHOR_USER_NAME = "LuckyRathod"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = []
+LIST_OF_REQUIREMENTS = [
+    "dvc",
+    "tqdm",
+    "tensorflow",
+    "joblib"
+]
 
 
 setup(
     name=SRC_REPO,
-    version="0.0.1",
+    version="0.0.3",
     author=AUTHOR_USER_NAME,
     description="A small package for DVC",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="luckyrathod46@gmail.com",
+    author_email="luckyrathod46@@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
